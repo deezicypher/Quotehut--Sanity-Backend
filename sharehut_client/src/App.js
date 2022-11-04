4b4bf6ef34cf69b055f636e3f85091297b0eab85
deezitheviper <amuhdavid@gmail.com>
2022-11-04 02:06:15 +0100

    react client

---
+++ sharehut_client/src/App.js
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:( <div className='text-3xl font-bold underline'>
+    Hello world!
+  </div>)
+  }
+]) 
+
+function App() {
+  return (
+   <RouterProvider router={router} />
+  );
+}
+
+export default App;
