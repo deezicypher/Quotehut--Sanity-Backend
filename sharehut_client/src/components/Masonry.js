248ecb7adb2433c6bb1686b8000414098b9d7c75
deezitheviper <amuhdavid@gmail.com>
2022-11-08 23:54:28 +0100

    masonry added

---
+++ sharehut_client/src/components/Masonry.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Masonry from 'react-masonry-css';
+import Pin from './Pin';
+
+
+
+const MasonryGrid = ({pins}) => {
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 1
+      };
+
+    return (
+        <div>
+            <Masonry
+  breakpointCols={breakpointColumnsObj}
+  className="flex animate-slide-fwd"
+
+>
+{pins.map(pin => (
+    <Pin key={pin?._id} pin={pin} />
+))}
+
+</Masonry>
+        </div>
+    );
+};
+
+export default MasonryGrid;
